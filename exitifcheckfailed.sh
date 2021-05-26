892d98f7a6463e92545da19db28f0f6664417116
zhopen <zh_open@163.com>
2021-05-26 16:28:22 +0800

    exitifcheckfailed.sh

---
+++ exitifcheckfailed.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+#检查某个程序是否运行，如果不运行就退出。
+
+ok=true
+for program in solana-{faucet,genesis,keygen,validator}; do
+  $program -V || ok=false
+done
+$ok || {
+  echo
+  echo "Unable to locate required programs.  Try building them first with:"
+  echo
+  echo "  $ cargo build --all"
+  echo
+  exit 1
+}
